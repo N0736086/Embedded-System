dfaafde5a3eef174b31f2d7e39451a471fed7973
N0736086 <35261490+N0736086@users.noreply.github.com>
2018-05-31 15:52:30 +0100

    Add files via upload

---
+++ Receiver_FD.ino
@@ -0,0 +1,158 @@
+// RECEIVER BOARD 
+int Level1 = 11;        //INITIALISING RED LED PIN
+int Level2 = 12;        //INITIALISING YELLOW LED PIN
+int Level3 = 13;        //INITIALISING GREEN LED PIN
+//int Level4 = 10;        //INITIALISING RED LED PIN
+//int Level5 = 11;        //INITIALISING YELLOW LED PIN
+//int Level6 = 12;        //INITIALISING GREEN LED PIN
+
+int BUZZER = 8;      //INITIALISING BUZZER PIN
+int pinA = 0;
+int pinB = 1;
+int pinC = 2;
+int pinD = 3;
+int pinE = 4;
+int pinF = 5;
+int pinG = 6;
+int D1 = 7;
+int D2 = 8;
+int D3 = 9;
+int D4 = 10;
+char receive;                            //INITIALISING A MODE VARIABLE TO STORE MODE DATA COMING FROM TRANSMITTER
+ 
+void setup()                          //SETTING UP/DECLARING OF ARDUINO PINS, EITHER OUTPUT OR INPUT AND SERIAL MONITORS
+{
+  pinMode(pinA, OUTPUT);
+  pinMode(pinB, OUTPUT);
+  pinMode(pinC, OUTPUT);
+  pinMode(pinD, OUTPUT);
+  pinMode(pinE, OUTPUT);
+  pinMode(pinF, OUTPUT);
+  pinMode(pinG, OUTPUT);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+
+
+  
+  pinMode(Level1, OUTPUT);              //DECLARE RED LED PIN AS OUTPUT PIN
+  pinMode(Level2, OUTPUT);              //DECLARE YELLOW LED PIN AS OUTPUT PIN
+  pinMode(Level3, OUTPUT);              //DECLARE GREEN LED PIN AS OUTPUT PIN
+//  pinMode(Level4, OUTPUT); 
+//  pinMode(Level5, OUTPUT);              //DECLARE GREEN LED PIN AS OUTPUT PIN
+//  pinMode(Level6, OUTPUT); 
+  Serial.begin(9600);                 //INITIALISE SERIAL MONITOR OF RECEIVER BOARD TO TRANSMIT CODE TO THE BOARD  
+  Serial1.begin(9600);
+  while (!Serial1);//DECLARE BUZZER PIN AS OUTPUT PIN
+}
+
+void loop()                           //STARTING ARDUINO LOOP
+{                                     //LOOP STARTS
+
+  while(Serial1.available())           //CHECKS IF THERE IS ANY VALUE FROM THE ZIGBEE CHANNEL
+  {
+    receive = Serial1.read();              //READS THE ZIGBEE CHANNEL VALUE AND STORES THEM IN THE "Mode" VARIABLE
+  digitalWrite(D4, HIGH);
+   if(receive=='1')                    //IF ZIGBEE CHANNEL IS READ AS 'R'
+      {                               //REGULAR MODE STARTS
+        //YELLOW LED IS OFF
+      digitalWrite(Level1, HIGH);
+      digitalWrite(Level2, LOW); 
+      digitalWrite(Level3, LOW); 
+        delay(100);
+        digitalWrite(Level1, LOW);
+
+      }                               //REGULAR MODE ENDS
+      
+      else if(receive=='2')                    //IF ZIGBEE CHANNEL IS READ AS 'R'
+      {                               //REGULAR MODE STARTS
+        //YELLOW LED IS OFF
+      digitalWrite(Level1, LOW);
+      digitalWrite(Level2, HIGH); 
+      digitalWrite(Level3, LOW); 
+
+      }                
+
+      else if(receive=='3')                    //IF ZIGBEE CHANNEL IS READ AS 'R'
+      {                               //REGULAR MODE STARTS
+        //YELLOW LED IS OFF
+      digitalWrite(Level1, LOW);
+      digitalWrite(Level2, LOW); 
+      digitalWrite(Level3, HIGH); 
+            delay(50);
+      digitalWrite(Level3, LOW); 
+
+
+      }                
+
+    if(receive=='8')                    //IF ZIGBEE CHANNEL IS READ AS 'R'
+      {   
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW); 
+  //0
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(300);               // wait for a second
+  
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW); 
+  //1
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, LOW);   
+  digitalWrite(pinC, HIGH);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);   
+  delay(300);               // wait for a second
+  
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, HIGH);
+  digitalWrite(D4, LOW); 
+  //2
+  digitalWrite(pinA, LOW);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, HIGH);   
+  digitalWrite(pinD, LOW);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, LOW);   
+  digitalWrite(pinG, LOW);     
+  delay(300);               // wait for a second
+
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, HIGH); 
+  //3
+  digitalWrite(pinA, HIGH);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, LOW);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, LOW);   
+  digitalWrite(pinF, HIGH);   
+  digitalWrite(pinG, LOW);     
+  delay(300);               // wait for a second 
+  digitalWrite(pinC, HIGH);  
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinG, HIGH);
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW);       
+      }
+  }
+}
+//MAIN LOOP ENDS
+
+
---
+++ Transmitter_FD.ino
@@ -0,0 +1,93 @@
+#include <stdio.h> // Including the standard input/output library
+#include <Servo.h> //Including the Servo motor library within involves PWM 
+Servo myservo;  // create servo object to control a servo
+
+#define ECHOPIN 3 //defining pin for the ultrasonic range finder echo pin
+#define TRIGPIN 2 //defining pin for the ultrasonic range finder trigger pin 
+int SERVO = 7; //declaration for right servo motor on pin 4
+
+int buzzer = 9;
+
+int SWITCH = 8;
+void setup() 
+{  
+  pinMode(SWITCH, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(TRIGPIN, OUTPUT);           //DECLARE TRIGGERPIN AS OUTPUT PIN
+  pinMode(ECHOPIN, INPUT);            //DECLARE ECHOPIN AS INPUT PIN
+ // Serial.begin(9600);
+  Serial1.begin(9600);  //BEGENING OF THE ZIGBEE CHANNEL
+}
+int Val;
+void loop()
+{
+ //start front distance computing
+ digitalWrite(TRIGPIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIGPIN,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGPIN,LOW); 
+ float distance = pulseIn(ECHOPIN, HIGH)/58; // return 0 if no high pulse in 1sec
+// Serial.print("Front Distance: ");
+// Serial.print(distance);
+// Serial.println("cm");
+ delay(200);//end front distance
+
+int value = analogRead(A0);
+
+Val = digitalRead(SWITCH);
+Serial.println(value);
+ //Conditions start here 
+   
+if(distance >=0&&distance <=10)
+{
+  tone(buzzer, 100,500);
+  Serial1.write('1'); // Water Level 1
+  myservo.detach();
+}
+
+if(distance >10&&distance <=30)
+{
+  Serial1.write('2'); // Water Level 2
+//  myservo.attach(SERVO);
+}
+
+//if(distance >16&&distance <=24)
+//{
+//  Serial1.write('3'); // Water Level 2
+////  myservo.attach(SERVO);
+//}
+//if(distance >24&&distance <=32)
+//{
+//  Serial1.write('4'); // Water Level 2
+////  myservo.attach(SERVO);
+//}
+//
+//if(distance >32 && distance <=40)
+//{
+//  Serial1.write('5'); // Water Level 3
+////  myservo.attach(SERVO);
+//}
+
+if(distance >30 )
+{
+  Serial1.write('3'); //Water Level 4
+  myservo.attach(SERVO);
+  myservo.write(45);
+  tone(buzzer, 100,1000);
+  delay(100);
+}
+
+//if(Val == 0)
+//{
+//    Serial1.write('7'); //Water Level 4
+//
+//}
+else if(value <550)
+ {
+  Serial1.write('8'); //Water Level 4
+}
+}
+
+ 
+ 
